c275c668c8a4d4c442780c075e3fb53014772e2f
Twynzen <razzziel001@gmail.com>
2019-11-05 14:37:19 -0500

    Conexión a la bd con php

---
+++ vistas/Modulos/conexionBD.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ *
+ */
+class  ConexionBD{
+
+  public function cBD(argument)
+  {
+    //Esta clase pide 3 parametros 1nombre host, usuario bd y contrasena bd
+    $bd = new PDO("mysql:host-localhost;dbname=crud", "root","");
+
+    return $bd;
+  }
+}
+
+
+
+ ?>
